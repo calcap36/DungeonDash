331e7860a4d2922cd3ab68ab4b5e47817139d1be
calcap36 <82478261+calcap36@users.noreply.github.com>
2021-06-21 12:22:53 +1200

    slidefinish

--- Player.gd
+++ Player.gd
@@ -5,44 +5,56 @@ const SPEED = 60
 const GRAVITY = 9.8
 const JUMP_POWER = -230
 const FLOOR = Vector2(0, -1)
- 
 
 var velocity = Vector2()
 
 var on_ground = false
-
+var animation = "idle"
 func _physics_process(delta): 
 	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
-		$AnimatedSprite.play("run")
+		animation = "run"
 		$AnimatedSprite.flip_h = false 
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
-		$AnimatedSprite.play("run")
+		animation = "run"
 		$AnimatedSprite.flip_h = true
 	else:
 		velocity.x = 0
 		if on_ground == true:
-			$AnimatedSprite.play("idle")
+			animation = "idle"
 	
 	if Input.is_action_pressed("ui_up"):
 		if on_ground == true:
 			velocity.y = JUMP_POWER
 			on_ground = false 
-	 
+			
+	if Input.is_action_pressed("ui_down"):
+		animation = "slide"
+		$CollisionShape2D.disabled = true
+		$SlideCollider.disabled = false
+	else:
+		$CollisionShape2D.disabled = false
+		$SlideCollider.disabled = true
+
+	
+	
 	velocity.y += GRAVITY 	
+	
+
+
 	
 	if is_on_floor():
 		on_ground = true
 	else:
 		on_ground= false 
 		if velocity.y < 0:
-			$AnimatedSprite.play("jump")
+			animation = "jump"
 		else:
-			$AnimatedSprite.play("fall")
+			animation = "fall"
 		
 		
 		
-	
+	$AnimatedSprite.play(animation)
 	velocity = move_and_slide(velocity, FLOOR)
