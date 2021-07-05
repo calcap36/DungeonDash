e3bbace9f6316dfb973e0cfc8cf3203231e28b29
calcap36 <82478261+calcap36@users.noreply.github.com>
2021-07-06 10:50:01 +1200

    boulder1

---
+++ KinematicBody2D.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const GRAVITY = 9.8
+const SPEED = 30 
+const FLOOR = Vector2(0, -1)
+
+var velocity = Vector2()
+
+var direction = 1
+
+func _ready():
+	pass
+
+func  _physics_process(delta):
+	velocity.x = SPEED * direction
+	
+	$AnimatedSprite.play("Rock")
+	velocity.y += GRAVITY 
+	
+	
+	velocity = move_and_slide(velocity, FLOOR)
+	
+	if is_on_wall():
+		direction = direction *  -1 
+	
+	
+	
+	
