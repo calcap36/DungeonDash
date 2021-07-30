d1823b96a127dfa3bbda1ea6fa62758886051401
calcap36 <82478261+calcap36@users.noreply.github.com>
2021-07-30 13:24:08 +1200

    Dashstart

--- Player.gd
+++ Player.gd
@@ -10,7 +10,12 @@ const SPRINT = 120
 
 var velocity = Vector2()
 
+#dash
+var dashdirection = Vector2(1,0)
+var candash = false
+var dashing = false 
 
+#jump
 var on_ground = false
 var animation = "idle"
 func _physics_process(delta): 
@@ -27,7 +32,7 @@ func _physics_process(delta):
 		velocity.x = 0
 		if on_ground == true:
 			animation = "idle"
-	
+		
 	if Input.is_action_pressed("ui_up"):
 		if on_ground == true:
 			velocity.y = JUMP_POWER
@@ -61,7 +66,15 @@ func _physics_process(delta):
 	$AnimatedSprite.play(animation)
 	velocity = move_and_slide(velocity, FLOOR)
 	
-
+func dash():
+	if is_on_floor():
+		candash = true 
+	if Input.is_action_pressed("ui_right"):
+		dashdirection = Vector2(1,0)
+	if Input.is_action_pressed("ui_left"):
+		dashdirection = Vector2(-1,0)
+		
+		
 
 	
 	 
